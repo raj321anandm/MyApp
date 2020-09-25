d0a9cb107d8aa4fb9d7f46831cf734ffefe49ff0
raj321anandm <66445387+raj321anandm@users.noreply.github.com>
2020-09-25 21:59:08 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='mypackage',
+        version='0.1',
+        description='Testing installation of package',
+        url='#',
+        author='Anand Raj.M',
+        author_email='anandraj.m@rajalakshmi.edu.in',
+        license='MIT',
+        packages=['mypackage'],
+        zip_safe=False)
+
---
+++ test.py
@@ -0,0 +1,4 @@
+from mypackage import Apower, Average, SayHello
+SayHello("World")
+x = Apower(3,2)
+print("power(3,2) :", x)
